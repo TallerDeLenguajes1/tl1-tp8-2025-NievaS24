0780bce377cfa92ac947fb3669a5a2889b96abae
NievaS24 <santinieva24@gmail.com>
2025-06-03 11:47:20 -0300

    Comienzo de ejercicio 1.

---
+++ ToDo/ToDo.cs
@@ -0,0 +1,9 @@
+namespace EspacioTarea
+{
+    public class Tarea {
+        public int TareaID {get; set;} //Numero en ciclo iterativo
+        public string Descripcion {get; set;}
+        public int Duracion {get; set;} // Entre 10 - 100
+    }
+    
+}
