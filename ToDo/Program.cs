7791e6e2454f8b327b77d5adc7aa9b610fef8d1c
NievaS24 <santinieva24@gmail.com>
2025-06-03 13:35:02 -0300

    Mostrar listas realizado

--- ToDo/Program.cs
+++ ToDo/Program.cs
@@ -7,18 +7,18 @@ int duracion, N;
 bool conversionPosible;
 do
 {
-    Console.WriteLine("Ingrese el numero de tareas que desea crear:");
+    Console.WriteLine("\n---- Ingrese el numero de tareas que desea crear ----\n");
     conversionPosible = int.TryParse(Console.ReadLine(), out N);
 } while (!conversionPosible || N <= 0);
 for (int i = 0; i < N; i++)
 {
     Tarea MiTarea = new Tarea();
     MiTarea.TareaID = i + 1;
-    Console.WriteLine($"Ingrese una descripcion para la tarea nº {i + 1}:");
+    Console.WriteLine($"\n---- Ingrese una descripcion para la tarea Nº {i + 1} ----\n");
     MiTarea.Descripcion = Console.ReadLine();
     do
     {
-        Console.WriteLine($"Ingrese la duracion de la tarea nº {i + 1}(entre 10 y 100):");
+        Console.WriteLine($"\n---- Ingrese la duracion de la tarea Nº {i + 1}(entre 10 y 100 minutos) ----\n");
         conversionPosible = int.TryParse(Console.ReadLine(), out duracion);
     } while (!conversionPosible || duracion < 10 || duracion > 100);
     MiTarea.Duracion = duracion;
@@ -26,13 +26,13 @@ for (int i = 0; i < N; i++)
 }
 //Interfaz para mover las tareas pendientes a realizadas
 int ID;
-Console.WriteLine("Si deseas marcar una tarea como realizada presione [1]:");
+Console.WriteLine("\n---- Si deseas marcar una tarea como realizada presione [1] ----\n");
 int.TryParse(Console.ReadLine(), out int decision);
 while (decision == 1)
 {
     do
     {
-        Console.WriteLine("Ingrese el ID de la tarea:");
+        Console.WriteLine("\n---- Ingrese el ID de la tarea ----\n");
         conversionPosible = int.TryParse(Console.ReadLine(), out ID);
     } while (!conversionPosible);
     Tarea tareaARealizar = tareasPendientes.Find(tarea => tarea.TareaID == ID);
@@ -40,32 +40,44 @@ while (decision == 1)
     {
         tareasRealizadas.Add(tareaARealizar);
         tareasPendientes.Remove(tareaARealizar);
-        Console.WriteLine("Tarea marcada como realizada.");
+        Console.WriteLine("\n O O O O Tarea marcada como realizada O O O O\n");
     }
     else 
     {
-        Console.WriteLine("El ID ingresado no existe o la tarea ya fue realizada.");
+        Console.WriteLine("\n X X X X El ID ingresado no existe o la tarea ya fue realizada X X X X\n");
     }
-    Console.WriteLine("Si deseas marcar una tarea como realizada presione [1]:");
+    Console.WriteLine("\n---- Si deseas marcar una tarea como realizada presione [1] ----\n");
     int.TryParse(Console.ReadLine(), out decision);
 }
-Console.WriteLine("Si deseas buscar una tarea pendiente por su descripcion presione [1]:");
+Console.WriteLine("\n---- Si deseas buscar una tarea pendiente por su descripcion presione [1] ----\n");
 int.TryParse(Console.ReadLine(), out decision);
 //Busqueda de tareas pendientes por descripcion
 string descripcionBuscada;
 while (decision == 1)
 {
-    Console.WriteLine("Ingrese la descripcion que quiere buscar:");
+    Console.WriteLine("\n---- Ingrese la descripcion que quiere buscar ----\n");
     descripcionBuscada = Console.ReadLine();
     Tarea tareaBuscada = tareasPendientes.Find(tarea => tarea.Descripcion == descripcionBuscada);
     if (tareaBuscada != null)
     {
-        Console.WriteLine($"Tarea encontrada: \n ID: {tareaBuscada.TareaID} \n Descripcion: {tareaBuscada.Descripcion} \n Duracion: {tareaBuscada.Duracion}");
+        Console.WriteLine($"\n O O O O Tarea encontrada O O O O \n\t ID: {tareaBuscada.TareaID} \n\t Descripcion: {tareaBuscada.Descripcion} \n\t Duracion: {tareaBuscada.Duracion}");
     }
     else
     {
-        Console.WriteLine("No se ha encontrado una tarea con esa descripcion.");
+        Console.WriteLine("\n X X X X No se ha encontrado una tarea con esa descripcion X X X X\n");
     }
-    Console.WriteLine("Si deseas buscar una tarea pendiente por su descripcion presione [1]:");
+    Console.WriteLine("\n---- Si deseas buscar una tarea pendiente por su descripcion presione [1] ----\n");
     int.TryParse(Console.ReadLine(), out decision);
+}
+//Mostrar listas
+Console.WriteLine("Tareas Pendientes:");
+for (int i = 0; i < tareasPendientes.Count; i++)
+{
+    Console.WriteLine($"\t Tarea {i}: \n\t\t ID: {tareasPendientes[i].TareaID} \n\t\t Descripcion: {tareasPendientes[i].Descripcion} \n\t\t Duracion: {tareasPendientes[i].Duracion}");
+}
+
+Console.WriteLine("Tareas Realizadas:");
+for (int i = 0; i < tareasRealizadas.Count; i++)
+{
+    Console.WriteLine($"\t Tarea {i}: \n\t\t ID: {tareasRealizadas[i].TareaID} \n\t\t Descripcion: {tareasRealizadas[i].Descripcion} \n\t\t Duracion: {tareasRealizadas[i].Duracion}");
 }
